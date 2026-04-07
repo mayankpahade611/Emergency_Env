27dc0a5a2fc19e1c7c06689ca3b5c606c3c086aa
mayankpahade611 <mayankpahade4@gmail.com>
2026-04-07 14:42:29 +0530

    Fix environment variables

--- inference.py
+++ inference.py
@@ -12,8 +12,8 @@ from env.models import Action
 load_dotenv()
 
 API_KEY = os.getenv("HF_TOKEN")
-API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
-MODEL_NAME = os.getenv("MODEL_NAME", "Qwen/Qwen2.5-72B-Instruct")
+API_BASE_URL = os.getenv("API_BASE_URL")
+MODEL_NAME = os.getenv("MODEL_NAME")
 
 MAX_STEPS = 3
 
